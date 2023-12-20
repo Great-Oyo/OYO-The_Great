d3019d41d9fe342a1566642dbce70d44f9e87f6b
Great-Oyo <153543569+Great-Oyo@users.noreply.github.com>
2023-12-20 07:05:30 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,15 +2,13 @@ let fly = [];
 let fly2;
 function setup() {
 
-createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth, windowHeight);
 	background(100, 200, 255);
       rectMode(CENTER);
   colorMode(HSB);
   angleMode(DEGREES);	// change the angle mode from radians to degrees
 }
-var scalar = 50;  // set the radius of circle
-var startX = windowWidth/2;	// set the x-coordinate for the circle center
-var startY = windowHeight/2;	// set the y-coordinate for the circle center
+
 f1 = 0;
 f2 = 0;
 f3 = 0;
@@ -24,6 +22,9 @@ f = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 
 function draw() {
+  var scalar = 50;  // set the radius of circle
+var startX = width/2;	// set the x-coordinate for the circle center
+var startY = height/2;	// set the y-coordinate for the circle center
 
       a = 9;
   c_inc = 255/9;
@@ -32,7 +33,7 @@ function draw() {
   c3 = 255;
   
   for (let i = 0; i < a; i++){
-        b = (360 * i/9) - 50;
+        b = (360 * i/9) - 50
       var x = startX + scalar * cos(b);
   var y = startY + scalar * sin(b);
         noStroke();
@@ -42,7 +43,7 @@ function draw() {
     c3 -= c_inc;
     fly[i] = new Bug(x, y, 25);
     fly[i].display();
-  //    angle++;
+  //    angle++
   }
     fly2 = new Bug(width * (3/4), height * (3/4), 50, 50);
     fly2.display();
