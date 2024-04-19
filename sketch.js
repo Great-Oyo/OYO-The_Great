e433fbd47022e05f43f33d367393fd7779ca4c59
Great-Oyo <153543569+Great-Oyo@users.noreply.github.com>
2024-04-19 17:27:50 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -3,12 +3,13 @@ let fly2;
 function setup() {
 
   createCanvas(windowWidth, windowHeight);
-	background(100, 200, 255);
+
       rectMode(CENTER);
+  colorMode(HSB);
+  	background(200, 100, 255);
   angleMode(DEGREES);	// change the angle mode from radians to degrees
   fill(0);
   ellipse(width/2, height/2, 50, 50);
-
 }
 
 f1 = 0;
@@ -54,134 +55,179 @@ var startY = height/2;	// set the y-coordinate for the circle center
     if (dist(mouseX, mouseY, fly2.x, fly2.y) < fly2.w1) {
 
   if(f[0] & f[2] & f[3] & f[4] & f[5] & f[6] & f[7] & f[8] == 1){
-	fill(100, 200, 255);
-    	rect(100, 120, 50, 100);
+        fill(200, 100, 255);
+    	rect(100, 120, 275, 100);
+        fill(0);
 text( 'Beorc was formed', 100, 120);
 f = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 }
   if(f[0] & f[1] & f[2] & f[3] & f[5] & f[6] & f[7] & f[8]== 1){
-	fill(100, 200, 255);
-    	rect(100, 120, 50, 100);
+        fill(200, 100, 255);
+    	rect(100, 120, 275, 100);
+        fill(0);
 text( 'Mannaz was formed', 100, 120);
 f = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 }
   if(f[0] & f[1] & f[2] & f[4] & f[6] & f[7] & f[8]== 1){
-	fill(100, 200, 255);
-    	rect(100, 120, 50, 100);
+        fill(200, 100, 255);
+    	rect(100, 120, 275, 100);
+        fill(0);
 text( 'Othel was formed', 100, 120);
 f = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 } 
       
   if(f[0] & f[1] & f[2] & f[4] & f[5] & f[6] & f[7]== 1){
-	fill(100, 200, 255);
-    	rect(100, 120, 50, 100);
+        fill(200, 100, 255);
+    	rect(100, 120, 275, 100);
+        fill(0);
 text( 'Daeg was formed', 100, 120);
 f = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 }    
   if(f[0] & f[1] & f[2] & f[3] & f[5] & f[6] & f[7]== 1){
-    	background(100);
+        fill(200, 100, 255);
+    	rect(100, 120, 275, 100);
+        fill(0);
 text( 'Ehwaz was formed', 100, 120);
 f = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 }
   if(f[0] & f[1] & f[3] & f[5] & f[7] & f[8]== 1){
-    	background(100);
+        fill(200, 100, 255);
+    	rect(100, 120, 275, 100);
+        fill(0);
 text( 'Ing was formed', 100, 120);
 f = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 }
       
   if(f[0] & f[2] & f[4] & f[6] & f[8] == 1){
-    	background(100);
+        fill(200, 100, 255);
+    	rect(100, 120, 275, 100);
+        fill(0);
 text( 'Jera was formed', 100, 120);
 f = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 }
   if(f[0] &f[3] & f[5] & f[6] & f[7] == 1){
-    	background(100);
+        fill(200, 100, 255);
+    	rect(100, 120, 275, 100);
+        fill(0);
 text( 'Perdhro was formed', 100, 120);
 f = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 }
   if(f[8] & f[4] & f[0] & f[1] == 1){
-    	background(100);
+        fill(200, 100, 255);
+    	rect(100, 120, 275, 100);
+        fill(0);
 text( 'Fehu was formed', 100, 120);
 f = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 }
   if(f[1] & f[2] & f[4] & f[7] == 1){
-    	background(100);
+        fill(200, 100, 255);
+    	rect(100, 120, 275, 100);
+        fill(0);
 text( 'Uruz was formed', 100, 120);
 f = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 }
   if(f[1] & f[2] & f[8] & f[4] == 1){
-    	background(100);
+        fill(200, 100, 255);
+    	rect(100, 120, 275, 100);
+        fill(0);
 text( 'Ansuz was formed', 100, 120);
 f = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 }
   if(f[1] & f[3] & f[4] & f[8] == 1){
-    	background(100);
+        fill(200, 100, 255);
+    	rect(100, 120, 275, 100);
+        fill(0);
 text( 'Raidho was formed', 100, 120);
 f = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 }
   if(f[0] & f[2] & f[5] & f[7] == 1){
-    	background(100);
+        fill(200, 100, 255);
+    	rect(100, 120, 275, 100);
+        fill(0);
 text( 'Gifu was formed', 100, 120);
 f = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 }
   if(f[0] & f[2] & f[4] & f[7] == 1){
-    	background(100);
+        fill(200, 100, 255);
+    	rect(100, 120, 275, 100);
+        fill(0);
 text( 'Hagall was formed', 100, 120);
 f = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 }
   if(f[1] & f[4] & f[6] & f[8] == 1){
-    	background(100);
+        fill(200, 100, 255);
+    	rect(100, 120, 275, 100);
+        fill(0);
 text( 'Eihwaz was formed', 100, 120);
 f = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 }
   if(f[2] & f[3] & f[7] & f[8] == 1){
-    	background(100);
+        fill(200, 100, 255);
+    	rect(100, 120, 275, 100);
+        fill(0);
 text( 'Neid was formed', 100, 120);
 f = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 }
   if(f[0] & f[1] & f[3] & f[8] == 1){
-    	background(100);
+        fill(200, 100, 255);
+    	rect(100, 120, 275, 100);
+        fill(0);
 text( 'Wunjo was formed', 100, 120);
 f = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 }
   if(f[0] & f[4] & f[7] & f[8] == 1){
-    	background(100);
+        fill(200, 100, 255);
+    	rect(100, 120, 275, 100);
+        fill(0);
 text( 'Eolh was formed', 100, 120);
 f = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 }
   if(f[0] & f[3] & f[7] & f[8] == 1){
-    	background(100);
+        fill(200, 100, 255);
+    	rect(100, 120, 275, 100);
+        fill(0);
 text( 'Tir was formed', 100, 120);
 f = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 }
   if(f[0] & f[1] & f[4] & f[6] == 1){
-    	background(100);
+        fill(200, 100, 255);
+    	rect(100, 120, 275, 100);
+        fill(0);
 text( 'Sigel was formed', 100, 120);
 f = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 }
    if(f[1] & f[3] & f[8] == 1){
-    	background(100);
+        fill(200, 100, 255);
+    	rect(100, 120, 275, 100);
+        fill(0);
 text( 'Lagaz was formed', 100, 120);
 f = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 }  
   if(f[1] & f[4] & f[8] == 1){
-    	background(100);
+        fill(200, 100, 255);
+    	rect(100, 120, 275, 100);
+        fill(0);
 text( 'Thurisaz was formed', 100, 120);
 f = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 }
   if(f[4] & f[6] & f[8] == 1){
-    	background(100);
+        fill(200, 100, 255);
+    	rect(100, 120, 275, 100);
+        fill(0);
 text( 'Kenaz was formed', 100, 120);
 f = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 }
   if(f[5] & f[2] & f[8]== 1){
-	fill(100, 200, 255);
-    	rect(100, 120, 100, 25);
+        fill(200, 100, 255);
+    	rect(100, 120, 275, 100);
+        fill(0);
 text( 'triangle was formed', 100, 120);
 f = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 }
   if(f[4] & f[8] == 1){
-    	background(100);
+        fill(200, 100, 255);
+    	rect(100, 120, 275, 100);
+        fill(0);
 text( 'Isa was formed', 100, 120);
 f = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 }   
